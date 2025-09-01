4338d556522bf30a8f40131ee932d3e9f838708a
Erin Xu <erinhua@umich.edu>
2025-09-01 14:17:09 -0400

    Restore polyres.py file - recovered from previous implementation

---
+++ src/glayout/primitives/polyres.py
@@ -0,0 +1,274 @@
+import numpy as np
+np.float_ = np.float64
+
+
+from gdsfactory.components import rectangle
+from gdsfactory import Component
+from glayout.pdk.mappedpdk import MappedPDK
+from glayout.primitives.via_gen import via_array
+from glayout.util.comp_utils import prec_ref_center, movey, align_comp_to_port, movex
+from glayout.util.port_utils import add_ports_perimeter
+from glayout.pdk.sky130_mapped import sky130_mapped_pdk
+from glayout.pdk.gf180_mapped import gf180_mapped_pdk
+from glayout.spice import Netlist
+from glayout.primitives.guardring import tapring
+
+def poly_resistor_netlist(
+    circuit_name: str,
+    model: str,
+    width: float,
+    length: float,
+    multipliers: int
+) -> Netlist :
+
+    ltop = (round(length, 2))*(1e-6)
+    wtop = (round(width, 2))*(1e-6)
+    mtop = multipliers
+
+    #source_netlist=""".subckt {model} r0 r1 """+f'\n l={ltop} w={wtop} '
+
+    #source_netlist += "\n.ends"
+
+    source_netlist="""\n.subckt {circuit_name} {nodes} """+f'l={ltop} w={wtop} m={mtop}'+"""
+XMAIN PLUS MINUS VSUBS {model} r_width={{w}} r_length={{l}} m={{m}}"""
+
+    source_netlist += "\n.ends {circuit_name}"
+
+
+
+    return Netlist(
+        circuit_name=circuit_name,
+        nodes=['PLUS', 'MINUS', 'VSUBS'],
+        source_netlist=source_netlist,
+        instance_format="X{name} {nodes} {circuit_name} l={length} w={width} m={multipliers}}",
+        parameters={
+            'model': model,
+            'length': ltop,
+            'width': wtop,
+            'multipliers': mtop,
+        }
+    )
+
+def poly_resistor(
+    pdk: MappedPDK,
+    length: float = 1.65,
+    width: float = 0.35,
+    fingers: int = 1,
+    tie_layers: tuple[str,str] = ("met2","met2"),
+    is_snake: bool = True,
+    n_type: bool = False,
+    silicided: bool = False
+) -> Component:
+    #poly_res = (66, 13)
+    sab = (49,0)
+    res_mk = (110,5)
+    p_res = Component()
+    contact_length = 2.2
+    separation = 0.21 + width
+    #Extend poly for contacts
+    ex_length = length + 2*contact_length
+    for i in range(0,fingers):
+        #poly resistor rectangle
+        p_rect = rectangle(size=(width,ex_length), layer=pdk.get_glayer("poly"), centered=True)
+        p_rect_ref = prec_ref_center(p_rect)
+        p_res.add(p_rect_ref)
+        movex(p_rect_ref, (i)*separation)
+        #Add li layer on top and bottom contacts
+        li_top = rectangle(size=(width,contact_length), layer=pdk.get_glayer("met1"), centered=True)
+        li_top_ref = prec_ref_center(li_top)
+        p_res.add(li_top_ref)
+        movey(li_top_ref, contact_length/2 + length/2)
+        movex(li_top_ref, (i)*separation)
+
+        li_bot = rectangle(size=(width,contact_length), layer=pdk.get_glayer("met1"), centered=True)
+        li_bot_ref = prec_ref_center(li_bot)
+        p_res.add(li_bot_ref)
+        movey(li_bot_ref, - contact_length/2 - length/2)
+        movex(li_bot_ref, (i)*separation)
+
+        ##Add unsalicide layer, if not silicided, use this block, otherwise skip
+        unsal = rectangle(size=((width*fingers+0.56),(length)), layer=sab, centered=True)
+        unsal_ref = prec_ref_center(unsal)
+        p_res.add(unsal_ref)
+
+        ##Add RES_MK layer
+        resmk = rectangle(size=((width*fingers+0.56),(length)), layer=res_mk, centered=True)
+        resmk_ref = prec_ref_center(resmk)
+        p_res.add(resmk_ref)
+
+        #Place poly to li via contact
+        licon1 = via_array(pdk, "poly", "met1", size=(width,contact_length))
+        licon1_ref = prec_ref_center(licon1)
+        #p_res.add(licon1_ref)
+        #movey(licon1_ref, contact_length/2 + length/2)
+
+        licon2 = via_array(pdk, "poly", "met1", size=(width,contact_length))
+        licon2_ref = prec_ref_center(licon2)
+        p_res.add(licon2_ref)
+        movey(licon2_ref, - contact_length/2 - length/2)
+        movex(licon2_ref, (i)*separation)
+
+        licon3 = via_array(pdk, "poly", "met1", size=(width,contact_length))
+        licon3_ref = prec_ref_center(licon3)
+        p_res.add(licon3_ref)
+        movey(licon3_ref, contact_length/2 + length/2)
+        movex(licon3_ref, (i)*separation)
+
+        # place metal 1 layer on contacts
+        met1_top = rectangle(size=(width,contact_length), layer=pdk.get_glayer("met2"), centered=True)
+        met1_top_ref = prec_ref_center(met1_top)
+        p_res.add(met1_top_ref)
+        movey(met1_top_ref, contact_length/2 + length/2)
+        movex(met1_top_ref, (i)*separation)
+
+        met1_bot = rectangle(size=(width,contact_length), layer=pdk.get_glayer("met2"), centered=True)
+        met1_bot_ref = prec_ref_center(met1_bot)
+        p_res.add(met1_bot_ref)
+        movey(met1_bot_ref, - contact_length/2 - length/2)
+        movex(met1_bot_ref, (i)*separation)
+        #place li to metal vias
+        met1con1 = via_array(pdk, "met1", "met2", size=(width,contact_length))
+        met1con1_ref = prec_ref_center(met1con1)
+        p_res.add(met1con1_ref)
+        movey(met1con1_ref, contact_length/2 + length/2)
+        movex(met1con1_ref, (i)*separation)
+
+        met1con2 = via_array(pdk, "met1", "met2", size=(width,contact_length))
+        met1con2_ref = prec_ref_center(met1con2)
+        p_res.add(met1con2_ref)
+        movey(met1con2_ref, - contact_length/2 - length/2)
+        movex(met1con2_ref, (i)*separation)
+
+        con_offset = (separation)/2
+        if is_snake == True:
+            if i > 0:
+                met1_connect = rectangle(size=(width+separation,contact_length), layer=pdk.get_glayer("met2"),centered= True)
+                met1_con_ref = prec_ref_center(met1_connect)
+                p_res.add(met1_con_ref)
+                if i%2 == 0:
+                    movey(met1_con_ref, - contact_length/2 - length/2)
+                    movex(met1_con_ref, (i-1)*separation+con_offset)
+                else:
+                    movey(met1_con_ref, contact_length/2 + length/2)
+                    movex(met1_con_ref, (i-1)*separation+con_offset)
+
+        if i == 0:
+            p_res.add_ports(met1_bot_ref.get_ports_list(), prefix="MINUS_")
+
+
+    tap_separation = max(
+            pdk.util_max_metal_seperation(),
+            pdk.get_grule("active_diff", "active_tap")["min_separation"],
+        )
+    tap_separation += pdk.get_grule("p+s/d", "active_tap")["min_enclosure"]
+    tap_encloses = (
+            2 * (tap_separation + p_res.xmax),
+            2 * (tap_separation + p_res.ymax),
+        )
+    tiering_ref = p_res << tapring(
+            pdk,
+            enclosed_rectangle=tap_encloses,
+            sdlayer="p+s/d",
+            horizontal_glayer=tie_layers[0],
+            vertical_glayer=tie_layers[1],
+        )
+    p_res.add_ports(tiering_ref.get_ports_list(), prefix="tie_")
+    
+    # add pplus or nplus layer according to the polyresistor type
+    if n_type:
+        plus_layer = pdk.get_glayer("n+s/d")  # N-plus for N-type polyresistor
+    else:
+        plus_layer = pdk.get_glayer("p+s/d")  # P-plus for P-type polyresistor
+    
+    plus = rectangle(size=(2*p_res.xmax+2,2*p_res.ymax+2), layer=plus_layer, centered=True)
+    plus_ref = prec_ref_center(plus)
+    p_res.add(plus_ref)
+    # add pwell
+    #p_res.add_padding(
+    #    layers=(pdk.get_glayer("pwell"),),
+    #    default=pdk.get_grule("pwell", "active_tap")["min_enclosure"],
+    #)
+    #p_res = add_ports_perimeter(p_res,layer=pdk.get_glayer("pwell"),prefix="well_")
+
+    #print(i)
+    if i%2 == 0:
+        p_res.add_ports(met1_top_ref.get_ports_list(), prefix="PLUS_")
+    else:
+        p_res.add_ports(met1_bot_ref.get_ports_list(), prefix="PLUS_")
+
+    # Select model based on type and silicidation
+    if n_type:
+        if silicided:
+            model = 'npolyf_s'  # n-type, silicided
+        else:
+            model = 'npolyf_u'  # n-type, unsalicided
+    else:
+        if silicided:
+            model = 'ppolyf_s'  # p-type, silicided
+        else:
+            model = 'ppolyf_u'  # p-type, unsalicided
+    
+    p_res.info['netlist'] = poly_resistor_netlist(
+        circuit_name="POLY_RES",
+        model=model,
+        width=width,
+        length=length,
+        multipliers=1,
+    )
+    #print(p_res.get_ports_list())
+    return p_res
+
+def add_polyres_labels(pdk: MappedPDK, p_res: Component, length, width, fingers):
+    p_res.unlock()
+    #met1_label = (68, 5)
+    #met1_pin = (68, 16)
+    move_info = list()
+    separation = 0.21 + width
+    contact_length = 2.2
+    p_pin = p_res << rectangle(size=(0.1,0.1),layer=pdk.get_glayer("met2"),centered=True)
+    if fingers%2 == 0:
+        movey(p_pin, -contact_length/2 - length/2)
+        movex(p_pin, (fingers-1)*separation)
+    else:
+        movey(p_pin, contact_length/2 + length/2)
+        movex(p_pin, (fingers-1)*separation)
+
+    m_pin = p_res << rectangle(size=(0.1,0.1),layer=pdk.get_glayer("met2"),centered=True)
+    movey(m_pin, -contact_length/2 - length/2)
+
+    #plus label
+    p_label = rectangle(layer=pdk.get_glayer("met2_pin"), size=(0.1,0.1), centered=True).copy()
+    p_label.add_label(text="PLUS",layer=pdk.get_glayer("met2_label"))
+    move_info.append((p_label,p_pin.ports["e1"],None))
+
+    m_label = rectangle(layer=pdk.get_glayer("met2_pin"), size=(0.1,0.1), centered=True).copy()
+    m_label.add_label(text="MINUS",layer=pdk.get_glayer("met2_label"))
+    move_info.append((m_label,m_pin.ports["e1"],None))
+
+    sub_label = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
+    sub_label.add_label(text="VSUBS",layer=pdk.get_glayer("met2_label"))
+    move_info.append((sub_label,p_res.ports["tie_N_top_met_N"], None))
+    for comp, prt, alignment in move_info:
+        alignment = ('c','b') if alignment is None else alignment
+        compref = align_comp_to_port(comp, prt, alignment=alignment)
+        p_res.add(compref)
+    return p_res.flatten()
+
+
+# Test different configurations
+print("Testing P-type, unsilicided polyresistor...")
+resistor = add_polyres_labels(gf180_mapped_pdk, poly_resistor(gf180_mapped_pdk, width=0.8, fingers=1, is_snake=True, n_type=False, silicided=False), 1.65, 0.8, 1) 
+resistor.show()
+resistor.name = "POLY_RES_P_UNSAL"
+magic_drc_result = gf180_mapped_pdk.drc_magic(resistor, resistor.name)
+lvs_result = gf180_mapped_pdk.lvs_netgen(resistor,resistor.name,copy_intermediate_files=True)
+print("P-type, unsilicided netlist:")
+print(resistor.info['netlist'].generate_netlist())
+
+# Test N-type, silicided
+print("\nTesting N-type, silicided polyresistor...")
+resistor_n_sal = add_polyres_labels(gf180_mapped_pdk, poly_resistor(gf180_mapped_pdk, width=0.8, fingers=1, is_snake=True, n_type=True, silicided=True), 1.65, 0.8, 1)
+resistor_n_sal.show()
+resistor_n_sal.name = "POLY_RES_N_SAL"
+print("N-type, silicided netlist:")
+print(resistor_n_sal.info['netlist'].generate_netlist())
